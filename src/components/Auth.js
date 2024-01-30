ec1f73096debb9e60f3bfd5adb8b3386b274ec97
DNieli <diegoantonio.nieli@gmail.com>
2024-01-30 12:53:57 +0000

    Finished functionality

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,19 +1,25 @@
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
-export const Auth = () => {
+export const Auth = (props) => {
+  const { setIsAuth } = props;
   const signInWithGoogle = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result);
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="auth">
-      <p>Sign in with Google to conitnue</p>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <p> Sign in with Google to continue </p>
+      <button onClick={signInWithGoogle}> Sign in with Google </button>
     </div>
   );
 };
